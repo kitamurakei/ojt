5177e068c15ec749fbb2df2ce42efb73af93462e
ToshihiroUchikoba <tnsrhmr@gmail.com>
2013-09-05 15:18:15 +0900

    ｺﾒﾝﾄを修正

--- java-practices/src/main/java/uchikoba/sampleBeanUtilis/SampleDetailBean.java
+++ java-practices/src/main/java/uchikoba/sampleBeanUtilis/SampleDetailBean.java
@@ -1,15 +1,13 @@
 package uchikoba.sampleBeanUtilis;
 
-
 public class SampleDetailBean {
-	
-	
+
 	private String s2;
-	
+
 	public String getS2() {
 		return s2;
 	}
-	
+
 	public void setS2(String s2) {
 		this.s2 = s2;
 	}
--- java-practices/src/main/java/uchikoba/sampleBeanUtilis/UseBeanUtilis.java
+++ java-practices/src/main/java/uchikoba/sampleBeanUtilis/UseBeanUtilis.java
@@ -6,21 +6,18 @@ import java.util.List;
 
 import org.apache.commons.beanutils.BeanUtilsBean;
 
-
 public class UseBeanUtilis {
-	
+
 	public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
-		
-		
+
 		SampleBean sample1 = new SampleBean();
 		List<SampleDetailBean> bean1 = new ArrayList<SampleDetailBean>();
 		SampleDetailBean db1 = new SampleDetailBean();
-		
+
 		db1.setS2("sampledeatai1");
 		bean1.add(db1);
 		sample1.setList(bean1);
-		
-		
+
 		SampleBean2 sample2 = new SampleBean2();
 		List<SampleDetailBean2> bean2 = new ArrayList<SampleDetailBean2>();
 		SampleDetailBean2 db2 = new SampleDetailBean2();
@@ -31,13 +28,13 @@ public class UseBeanUtilis {
 		db3.setS1("sampledeatai1にない");
 		db3.setS2("sampledeatai1にある");
 		bean2.add(db3);
-		
+
 		sample2.setList(bean2);
-		
+
 		BeanUtilsBean.getInstance().copyProperties(sample1, sample2);
 
 		System.out.println("sample1にSampleDetailBean2がｾｯﾄされちゃったよ");
-		
+
 	}
 
 }
